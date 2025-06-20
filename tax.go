05778f4b14806df0311299895a2b69de26d6bb2e
nikita <nikitakrut58@gmail.com>
2025-06-20 20:33:24 +0300

    pars imports

--- tax.go
+++ tax.go
@@ -1,35 +1,37 @@
-package tax
+package main
 
 import (
+	"fmt"
 	"go/parser"
 	"go/token"
-	"log"
 	"os"
 	"path/filepath"
 )
 
-func GetNamesPackages() {
+func getImportList() ([]string, error) {
 	fset := token.NewFileSet()
+	imports := make([]string, 0)
+
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil || !info.Mode().IsRegular() || filepath.Ext(info.Name()) != ".go" {
-			return nil // пропускаем файлы, не относящиеся к Go
+			return nil
 		}
 
 		file, err := parser.ParseFile(fset, path, nil, parser.AllErrors|parser.ImportsOnly)
 		if err != nil {
-			log.Printf("Ошибка парсинга файла %q: %v\n", path, err)
-			return nil
+			return fmt.Errorf("File parsing error %q: %v\n", path, err)
 		}
 
 		for _, imp := range file.Imports {
-			if imp.Path.Value != "" && imp.Path.Value[0] == '"' { // проверка валидности пути импорта
-				log.Printf("Импортированный пакет: %s\n", imp.Path.Value[1:len(imp.Path.Value)-1])
+			if imp.Path.Value != "" && imp.Path.Value[0] == '"' {
+				imports = append(imports, imp.Path.Value[1:len(imp.Path.Value)-1])
 			}
 		}
 		return nil
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	return imports, nil
 }
